52f830a61c5f89043aa57a1bd83676890942398f
tamaraalbilleh <tamara2albella@gmail.com>
2021-06-27 18:00:35 +0300

    done with code challenge 31

--- javascript-401d9/challenges/Data-Structures/repeatedWord/repeatedWord.js
+++ javascript-401d9/challenges/Data-Structures/repeatedWord/repeatedWord.js
@@ -9,7 +9,7 @@ function hashmap (str){
   let array =  str.match(/\w+/g);
   for (let i = 0; i < array.length; i++) {
     let word = array[i].toLowerCase();
-    if (hashTable.get(word) === word) {
+    if (hashTable.get(word) == word) {
       return hashTable.get(word);
     } else {
       hashTable.add(word, word);
